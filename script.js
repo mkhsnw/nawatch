44fca37b757c5c84321da795dc8338f090d58632
mkhsnw <98505705+mkhsnw@users.noreply.github.com>
2023-07-11 18:20:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function check(form){
+  if(form.username.value == ""){
+    alert("Please enter a username")
+  }
+  if(form.password.value == ""){
+    alert("Please enter a password")
+  }
+  if(form.fname.value == ""){
+    alert("Please enter your first name")
+  }
+  if(form.lname.value == ""){
+    alert("Please enter your last name")
+  }
+}
